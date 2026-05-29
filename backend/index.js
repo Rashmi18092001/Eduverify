bad1c5295a7c3c702af171ff5ab98fea1b678430
Rashmi Jadhav <jrashmi853@gmail.com>
2026-05-29 13:50:37 +0530

    cors origin changes

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://192.168.100.103:5173",
-        "https://incandescent-hamster-29e356.netlify.app"
+        " https://eduverify-verification.netlify.app"
     ],
     credentials: true
 }))
